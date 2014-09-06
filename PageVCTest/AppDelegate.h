05fe3cbf78f077b5f817547c4aa2144bb7b1c3db
LJJ <solitudesaver@gmail.com>
2014-09-06 16:54:42 +0800

    Initial Commit

---
+++ PageVCTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PageVCTest
+//
+//  Created by LJJ on 9/6/14.
+//  Copyright (c) 2014 LJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PageVCTestTests/PageVCTestTests.m
@@ -0,0 +1,34 @@
+//
+//  PageVCTestTests.m
+//  PageVCTestTests
+//
+//  Created by LJJ on 9/6/14.
+//  Copyright (c) 2014 LJJ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PageVCTestTests : XCTestCase
+
+@end
+
+@implementation PageVCTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
